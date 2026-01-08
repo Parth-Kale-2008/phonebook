8448fcae794b8aebad79c1ec51b04a01577950ed
Parth-Kale-2008 <parthkale416@gmail.com>
2026-01-09 00:16:15 +0530

    Add files via upload

---
+++ Phonebook.java
@@ -0,0 +1,24 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+
+class Phonebook{
+    public static void main(String[] args) {
+        Hashtable<String,Long>ht = new Hashtable<String,Long>();
+
+        ht.put("Rehman Dakait",1244452704L);
+        ht.put("Uzair Baloch",24452671423L);
+        ht.put("Hamza Mazari",7858418949L);
+        ht.put("Aslam Chaudhary",7952855945L);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the name ");
+        String name = sc.nextLine().toLowerCase();
+    
+        if(ht.containsKey(name)){
+            System.out.println("found "+ht.get(name));
+        }else{
+            System.out.print("not found");
+        }
+
+    }
+}
